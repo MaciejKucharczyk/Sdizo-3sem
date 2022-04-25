5a86ade9101134e8cb49f80dd6e0e8ab4ab174e1
MaciejKucharczyk <kucharczykm01@gmail.com>
2022-04-25 15:45:39 +0000

    Działająca lista i pomiary czasów

--- Lista.cpp
+++ Lista.cpp
@@ -3,9 +3,76 @@
 #include <string>
 #include <iomanip>
 #include <windows.h>
-#include "Lista.h"
+#include "Query.cpp"
 
 using namespace std;
+
+struct ElemList
+{
+	ElemList* next, * prev;   // nastêpnik i poprzednik
+	int data;
+};
+
+class List
+{
+	ElemList* head, * tail;
+public:
+
+	List();
+	~List();
+
+	void displayMenu();
+
+	void loadFromFile(string filename);
+
+	ElemList* IsValueInList(int val);
+
+	void addValue(int index, int value);
+
+	void deleteFromList(int val);
+
+	void pop_front();
+
+	void pop_back();
+
+	void DeleteThis(ElemList* p);
+
+	void display();
+
+	void displayBackwards();
+
+	//	void generateList(int size);
+
+	ElemList* findTail(); //funkcja zwracajaca ogon listy
+
+	void push_front(int val);
+
+	void push_back(int val);
+
+	void push_whereIndex(int val, int in);
+
+	void eksperymenty();
+
+	void DeleteAll();
+
+
+};
+
+void List::displayMenu()
+{
+	cout << endl;
+	cout << "----- LISTA -----" << endl;
+	cout << "1.Wczytaj z pliku" << endl;
+	cout << "2.Usun" << endl;
+	cout << "3.Dodaj" << endl;
+	cout << "4.Znajdz" << endl;
+	cout << "5.Utworz losowo" << endl;
+	cout << "6.Wyswietl" << endl;
+	cout << "7.Test (pomiary)" << endl;
+	cout << "0.Powrot do menu" << endl;
+	cout << "Podaj opcje:";
+}
+
 
 List::List()
 {
@@ -15,10 +82,11 @@ List::List()
 // Usuwa listê z pamiêci
 List::~List()
 {
-	cout << "Nic nie robie" << endl;
+	while (head)
+		pop_front();
 }
 
-ElemList* List::findTail(ElemList* head) //funkcja zwracajaca ogon listy
+ElemList* List::findTail() //funkcja zwracajaca ogon listy
 {
 	if (head != NULL)
 	{
@@ -30,13 +98,16 @@ ElemList* List::findTail(ElemList* head) //funkcja zwracajaca ogon listy
 	return head;
 }
 
-ElemList* List::IsValueInList(ElemList* head, int val)
+ElemList* List::IsValueInList(int val)
 {
-	while (head != NULL)
+	ElemList* p;
+	p = head;
+
+	while (p != NULL)
 	{
-		if (head->data == val)
-			return head;
-		head = head->next;
+		if (p->data == val)
+			return p;
+		p = p->next;
 	}
 	return NULL;
 }
@@ -46,9 +117,36 @@ void List::addValue(int index, int value)
 
 }
 
-void List::deleteFromList(ElemList*& head, ElemList*& tail, int val)
+void List::pop_front()
 {
-	ElemList* p = IsValueInList(head, val);
+	if (head != NULL)
+		DeleteThis(head);
+
+}
+
+void List::pop_back()
+{
+	if (tail != NULL)
+		DeleteThis(tail);
+}
+
+void List::DeleteThis(ElemList* p)
+{
+		if (p->prev)
+			p->prev->next = p->next;
+		else
+			head = p->next;
+		if (p
+			->next)
+			p->next->prev = p->prev;
+		else
+			tail = p->prev;
+		delete p;
+}
+
+void List::deleteFromList(int val)
+{
+	ElemList* p = IsValueInList(val);
 	if (p != NULL)
 	{
 		if (p->prev != NULL)
@@ -64,22 +162,28 @@ void List::deleteFromList(ElemList*& head, ElemList*& tail, int val)
 	}
 }
 
-void List::display(ElemList* head)
+void List::display()
 {
-	while (head != NULL)
+	ElemList* p;
+	p = head;
+
+	while (p != NULL)
 	{
-		cout << head->data << " ";
-		head = head->next;
+		cout << p->data << " ";
+		p = p->next;
 	}
 	cout << endl;
 }
 
-void List::displayBackwards(ElemList* tail)
+void List::displayBackwards()
 {
-	while (tail != NULL)
+	ElemList* p;
+
+	p = tail;
+	while (p != NULL)
 	{
-		cout << tail->data << " ";
-		tail = tail->prev;
+		cout << p->data << " ";
+		p = p->prev;
 	}
 }
 
@@ -88,7 +192,7 @@ void List::displayBackwards(ElemList* tail)
 
 }*/
 
-void List::push_front(ElemList*& head, int val) //funkcja dodajaca element na poczatek listy
+void List::push_front(int val) //funkcja dodajaca element na poczatek listy
 {
 	ElemList* p = new ElemList;
 	p->data = val;
@@ -102,7 +206,7 @@ void List::push_front(ElemList*& head, int val) //funkcja dodajaca element na po
 		tail = p;
 }
 
-void List::push_back(ElemList*& tail, int val) //funkcja dodajaca element na koniec listy
+void List::push_back(int val) //funkcja dodajaca element na koniec listy
 {
 	ElemList* p = new ElemList;
 
@@ -110,9 +214,13 @@ void List::push_back(ElemList*& tail, int val) //funkcja dodajaca element na kon
 	p->prev = tail;
 	p->next = NULL;
 	tail = p;
+	if (p->prev)
+		p->prev->next = p;
+	else 
+		head = p;
 }
 
-void List::push_whereIndex(ElemList*& head, int val, int in) //funkcja dodajaca element na miejsce elementu o danym indeksie
+void List::push_whereIndex(int val, int in) //funkcja dodajaca element na miejsce elementu o danym indeksie
 {
 	ElemList* p = new ElemList;
 	ElemList* bufor1 = new ElemList;
@@ -122,31 +230,31 @@ void List::push_whereIndex(ElemList*& head, int val, int in) //funkcja dodajaca 
 	int counter = 0;
 	p = head;
 
-	while (head != NULL)
+	while (p != NULL)
 	{
 		if (counter == in)
 		{
-			bufor1->data = head->data;
-			head->data = val;
+			bufor1->data = p->data;
+			p->data = val;
 
-			while (head->next != NULL)
+			while (p->next != NULL)
 			{
-				bufor2->data = head->next->data;
-				head->next->data = bufor1->data;
+				bufor2->data = p->next->data;
+				p->next->data = bufor1->data;
 				bufor1->data = bufor2->data;
-				head = head->next;
+				p = p->next;
 			}
 			break;
 		}
 		else
 		{
-			head = head->next;
+			p = p->next;
 			counter++;
 		}
 	}
 }
 
-void List::loadFromFile(ElemList*&head, string filename)
+void List::loadFromFile(string filename)
 {
 	int lineNum = 1; // numer linii, z ktorej zczytywane sa dane
 	int size, liczba; // zmienna liczba przechowuje aktualnie zczytywana liczbe
@@ -157,7 +265,62 @@ void List::loadFromFile(ElemList*&head, string filename)
 	{
 		lineNum++;
 		File >> liczba;
-		push_front(head, liczba);
+		push_front(liczba);
 	}
 	File.close();
+}
+
+void List::DeleteAll()
+{
+	while (head)
+		pop_front();
+}
+
+void List::eksperymenty()
+{
+	float avgTime1 = 0;
+	float avgTime2 = 0;
+	cout << "Trwa wykonywanie pomiaru..." << endl;
+	loadFromFile("zzLiczby100000.txt");
+	cout << "100k" << endl;
+
+	ElemList* g;
+	g = head;
+
+	int licznik = 123;
+	int indeksik = 0; 
+	for (int i = 0; i < 2000; i++)
+	{	
+		// ======================================
+		ElemList* g;
+		g = head;
+		for (int q = 0; q < rand() % 100000; q++)
+		{
+			g = g->next;							// losowanie elementu do usuniecia
+			licznik = g->data;
+		}
+		//======================================= 
+
+		licznik = rand() % 10000 - 5000;
+		//indeksik = rand() %100000;
+		
+		start = read_QPC(); // poczatek pomiaru 
+		IsValueInList(licznik);
+		elapsed = read_QPC() - start; // koniec pomiaru
+		avgTime1 += (1000000.0 * elapsed) / frequency;
+
+		pop_front(); // usuniecie elementu, aby zachowac ten sam rozmiar listy 
+
+		for (int j = 0; j < 3000; j++) // petla zmieniajaca n pierwszych i n ostatnich elementow listy (n_max dobrane do wielkosci pliku)
+		{
+			pop_back();
+			push_front(rand()%10000);
+		}
+	}
+	avgTime1 = avgTime1/2000;
+	//avgTime2 = avgTime2 /5000;
+	cout << "Sredni czas znajdowania elementu [us]: " << avgTime1 <<endl;
+	//cout << "Sredni czas dodawania ostatniego elementu [us]: " << avgTime2 << endl;
+
+	DeleteAll(); // usuwanie listy
 }
